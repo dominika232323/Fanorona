c3c4bc5446e98d34e4de95fea1bf66c44956a69d
dominika232323 <dominikaboguszewska62@gmail.com>
2022-12-10 22:50:40 +0100

    Iniciated class Pawns

---
+++ classes/pawns.py
@@ -0,0 +1,16 @@
+class Pawns():
+    def __init__(self):
+        # tworzy pustą tablicę/matrycę board._length x board._width
+        pass
+
+    def starting_pawns(self):
+        # ustawia pionki na początek gry
+        pass
+
+    def pawns_after_move(self):
+        # aktualizuje tablice pionkow po ruchu gracza
+        pass
+
+    def check_for_winner(self):
+        # sprawdza czy wszystkie pionki jednego z graczy zostaly juz zbite
+        pass
