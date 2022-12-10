5bd1e90041b8db66a131da473ee30b96ab42a316
dominika232323 <dominikaboguszewska62@gmail.com>
2022-12-10 22:56:47 +0100

    Iniciated class Move

---
+++ classes/move.py
@@ -0,0 +1,11 @@
+class Move():
+    def __init__(self):
+        pass
+
+    def which_can_move(self, pawns):
+        # sprawdza ktore pionki moga sie ruszyc i zbic pionek przeciwnika
+        pass
+
+    def possible_combo(self, pawns):
+        # sprawdza czy mozna zrobic kombo
+        pass
