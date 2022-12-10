9be0068e8dffa90be72b822e08d6721692d740d3
dominika232323 <dominikaboguszewska62@gmail.com>
2022-12-10 14:43:53 +0100

    Initiated class Board

---
+++ classes/board.py
@@ -0,0 +1,11 @@
+MIN_LENGTH = 3
+MIN_WIDTH = 3
+
+
+class Board():
+    def __init__(self, length=9, width=5):
+        # min length = 3, min width = 3
+        # (length * width) & 2 == 1
+        # length % 2 == 1 and width % 2 == 1
+        # max length, max width = ?????
+        pass
