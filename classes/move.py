9d012cc44a0f672bcea685a1ffb9f8c3743547dd
dominika232323 <dominikaboguszewska62@gmail.com>
2022-12-13 10:55:20 +0100

    Iniciated class Move part 2

--- classes/move.py
+++ classes/move.py
@@ -2,10 +2,18 @@ class Move():
     def __init__(self):
         pass
 
-    def which_can_move(self, pawns):
-        # sprawdza ktore pionki moga sie ruszyc i zbic pionek przeciwnika
+    def which_can_move(self, pawns, turn):
+        # sprawdza, ktore pionki moga sie ruszyc, bo mają obok wolne miejsce
         pass
 
-    def possible_combo(self, pawns):
+    def which_can_hit(self, pawns, turn):
+        # sprawdza, ktore pionki z tych co mogą się ruszyć mają bicie
+        pass
+
+    def possible_combo(self, pawns, previous_move_type):
         # sprawdza czy mozna zrobic kombo
+        pass
+
+    def move_without_hits(self):
+        # jeśli nie ma żadnych bić, to to zachodzi
         pass
