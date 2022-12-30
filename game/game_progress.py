29b3487679b9b8fcbd9d903ce2cd516941a11518
dominika232323 <dominikaboguszewska62@gmail.com>
2022-12-30 17:27:30 +0100

    Iniciated game_progress and player_turns

---
+++ game/game_progress.py
@@ -0,0 +1,21 @@
+from source.board import Board
+from source.pawns import Pawns
+from source.move import Move
+from player_turns import (
+    player_turn,
+    computer_random,
+    computer_the_best
+)
+from configuration import (
+    FIRST_COLOR,
+    SECOND_COLOR,
+    EMPTY_COLOR
+)
+
+
+def game_progress(opponent):
+    pass
+
+
+def opponent_turn(opponent):
+    pass
---
+++ game/player_turns.py
@@ -0,0 +1,15 @@
+from source.board import Board
+from source.pawns import Pawns
+from source.move import Move
+
+
+def player_turn(pawn_color):
+    pass
+
+
+def computer_random(pawn_color):
+    pass
+
+
+def computer_the_best(pawn_color):
+    pass
