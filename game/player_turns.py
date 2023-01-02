a657f51d24f517acc346b0fa464a9c180a86d7b1
dominika232323 <dominikaboguszewska62@gmail.com>
2023-01-02 18:24:23 +0100

    Implemented get_best_pawn_and_empty_cords and find_longest_group_to_kill

--- game/player_turns.py
+++ game/player_turns.py
@@ -13,8 +13,8 @@ def computer_random(pawns, pawn_color):
     pawn_cords, empty_cords = get_random_pawn_and_empty_cords(pawns, pawn_color)
 
 
-def computer_the_best(pawns, pawn_color):
-    pass
+def computer_best(pawns, pawn_color):
+    pawn_cords, empty_cords = get_best_pawns_and_empty_cords(pawns, pawn_color)
 
 
 def get_random_pawn_and_empty_cords(pawns, pawn_color):
@@ -28,4 +28,32 @@ def get_random_pawn_and_empty_cords(pawns, pawn_color):
         pawn_cords = choice(list(hit.which_can_move()))
         empty_cords = choice(hit.where_can_move()[pawn_cords])
     return pawn_cords, empty_cords
+
+
+def get_best_pawns_and_empty_cords(pawns, pawn_color):
+    hit = Hit(pawns, pawn_color)
+    hitting_pawns = list(hit.which_can_hit())
+    if hitting_pawns:
+        pawn_by_w, empty_by_w, length_by_w = find_longest_group_to_kill(hit.which_hits_by_withdrawal())
+        pawn_by_a, empty_by_a, length_by_a = find_longest_group_to_kill(hit.which_hits_by_approach())
+        if length_by_w >= length_by_a:
+            return pawn_by_w, empty_by_w
+        else:
+            return pawn_by_a, empty_by_a
+    else:
+        pawn_cords = choice(list(hit.which_can_move()))
+        empty_cords = choice(hit.where_can_move()[pawn_cords])
+        return pawn_cords, empty_cords
+
+
+def find_longest_group_to_kill(dict_of_hits):
+    pawn_cords = (0, 0)
+    empty_cords = (0, 0)
+    len_group_to_kill = 0
+    for pawn_and_empty in dict_of_hits:
+        if len(dict_of_hits[pawn_and_empty]) > len_group_to_kill:
+            pawn_cords = pawn_and_empty[0]
+            empty_cords = pawn_and_empty[1]
+            len_group_to_kill = len(dict_of_hits[pawn_and_empty])
+    return pawn_cords, empty_cords, len_group_to_kill
 
--- tests/test_player_turns.py
+++ tests/test_player_turns.py
@@ -39,3 +39,59 @@ def test_get_random_pawn_and_empty_cords_without_hits():
     pawn_cords, empty_cords = get_random_pawn_and_empty_cords(pawns, FIRST_COLOR)
     assert pawn_cords in [(4, 1), (4, 3), (4, 6), (4, 7)]
     assert empty_cords in [(3, 1), (4, 2), (4, 0), (3, 3), (4, 4), (4, 2), (3, 5), (3, 6), (3, 7), (4, 5), (3, 7), (4, 8)]
+
+
+# ---------------------------------- find_longest_group_to_kill()
+
+
+def test_find_longest_group_to_kill():
+    pawns = Pawns(Board())
+    new_pawns = [
+        [SECOND_COLOR, SECOND_COLOR, SECOND_COLOR, SECOND_COLOR, EMPTY_COLOR, SECOND_COLOR, SECOND_COLOR, SECOND_COLOR,
+         SECOND_COLOR],
+        [SECOND_COLOR, SECOND_COLOR, SECOND_COLOR, EMPTY_COLOR, FIRST_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR,
+         EMPTY_COLOR],
+        [EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, FIRST_COLOR, FIRST_COLOR, SECOND_COLOR,
+         FIRST_COLOR],
+        [FIRST_COLOR, FIRST_COLOR, FIRST_COLOR, SECOND_COLOR, EMPTY_COLOR, FIRST_COLOR, FIRST_COLOR, FIRST_COLOR,
+         FIRST_COLOR],
+        [FIRST_COLOR, FIRST_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, FIRST_COLOR, FIRST_COLOR, FIRST_COLOR,
+         FIRST_COLOR]
+    ]
+    pawns.set_actual_pawns(new_pawns)
+
+    move = Hit(pawns, SECOND_COLOR)
+    assert find_longest_group_to_kill(move.which_hits_by_approach()) == ((3, 3), (3, 4), 4)
+
+
+# ---------------------------------- get_best_pawn_and_empty_cords()
+
+
+def test_get_best_pawn_and_empty_cords_with_hits():
+    pawns = Pawns(Board())
+    new_pawns = [
+        [SECOND_COLOR, SECOND_COLOR, SECOND_COLOR, SECOND_COLOR, EMPTY_COLOR, SECOND_COLOR, SECOND_COLOR, SECOND_COLOR, SECOND_COLOR],
+        [SECOND_COLOR, SECOND_COLOR, SECOND_COLOR, EMPTY_COLOR, FIRST_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR],
+        [EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, FIRST_COLOR, FIRST_COLOR, SECOND_COLOR, FIRST_COLOR],
+        [FIRST_COLOR, FIRST_COLOR, FIRST_COLOR, SECOND_COLOR, EMPTY_COLOR, FIRST_COLOR, FIRST_COLOR, FIRST_COLOR, FIRST_COLOR],
+        [FIRST_COLOR, FIRST_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, FIRST_COLOR, FIRST_COLOR, FIRST_COLOR, FIRST_COLOR]
+    ]
+    pawns.set_actual_pawns(new_pawns)
+
+    assert get_best_pawns_and_empty_cords(pawns, SECOND_COLOR) == ((3, 3), (3, 4))
+
+
+def test_get_best_pawn_and_empty_cords_without_hits():
+    pawns = Pawns(Board())
+    new_pawns = [
+        [SECOND_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, SECOND_COLOR, EMPTY_COLOR],
+        [SECOND_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, SECOND_COLOR, EMPTY_COLOR],
+        [EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR],
+        [EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR, EMPTY_COLOR],
+        [EMPTY_COLOR, FIRST_COLOR, EMPTY_COLOR, FIRST_COLOR, EMPTY_COLOR, EMPTY_COLOR, FIRST_COLOR, FIRST_COLOR, EMPTY_COLOR]
+    ]
+    pawns.set_actual_pawns(new_pawns)
+
+    pawn_cords, empty_cords = get_best_pawns_and_empty_cords(pawns, FIRST_COLOR)
+    assert pawn_cords in [(4, 1), (4, 3), (4, 6), (4, 7)]
+    assert empty_cords in [(3, 1), (4, 2), (4, 0), (3, 3), (4, 4), (4, 2), (3, 5), (3, 6), (3, 7), (4, 5), (3, 7), (4, 8)]
