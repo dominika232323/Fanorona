7562d7227b0e31877296d6cb2bb1b7e9b21f8c4d
dominika232323 <dominikaboguszewska62@gmail.com>
2023-01-03 22:41:26 +0100

    Implemented __init__ and possible_combo for Combo

--- source/combo.py
+++ source/combo.py
@@ -4,33 +4,46 @@ from source.move import Move
 from source.movement import Movement
 from source.pawns import Pawns
 from source.turn import Turn
+from configuration import (
+    FIRST_COLOR,
+    SECOND_COLOR,
+    EMPTY_COLOR
+)
 
 
 class Combo(Turn):
-    def __init__(self, pawns, turn):
+    def __init__(self, pawns, turn, previous_pawn, previous_empty):
         super().__init__(pawns, turn)
-        self.move = Move(self.pawns, turn)
-        self.hit = Hit(self.pawns(), self.turn())
+        self._previous_pawn = previous_pawn
+        self._previous_empty = previous_empty
+        self._previous_move_type = Movement.recognize_move(previous_pawn, previous_empty)
+        self.hit = Hit(pawns, turn)
+        self.move = Move(pawns, turn)
+        self._new_pawn = previous_empty
 
-    def possible_combo(self, pawn, empty):
-        if not self._if_can_hit(pawn, empty):
+    @property
+    def previous_pawn(self):
+        return self._previous_pawn
+
+    @property
+    def previous_empty(self):
+        return self._previous_empty
+
+    @property
+    def previous_move_type(self):
+        return self._previous_move_type
+
+    @property
+    def new_pawn(self):
+        return self._new_pawn
+
+    def possible_combo(self):
+        if self._new_pawn not in self.hit.which_can_hit():
             return False
-        pawns_after_move = self.move.move_with_hits(pawn, empty)
-        self.pawns.set_actual_pawns(pawns_after_move)
-        previous_move_type = Movement.recognize_move(pawn, empty)
-        new_pawn_cords = empty
-        if new_pawn_cords in self.hit.which_can_hit():
-            for empty_space in self.hit.where_can_hit()[new_pawn_cords]:
-                if Movement.recognize_move(new_pawn_cords, empty_space) != previous_move_type:
-                    return True
+        for empty_space in self.hit.where_can_hit()[self._new_pawn]:
+            if Movement.recognize_move(self._new_pawn, empty_space) != self._previous_move_type:
+                return True
         return False
-
-    def _if_can_hit(self, pawn, empty):
-        if pawn not in self.hit.which_can_hit():
-            return False
-        if empty not in self.hit.where_can_hit()[pawn]:
-            return False
-        return True
 
     def make_combo(self):
         pass
