d743f825e50e77e70d24561a380717e25c7188fc
dominika232323 <dominikaboguszewska62@gmail.com>
2023-01-03 20:10:12 +0100

    Implemented some version of Combo

--- source/combo.py
+++ source/combo.py
@@ -7,15 +7,30 @@ from source.turn import Turn
 
 
 class Combo(Turn):
+    def __init__(self, pawns, turn):
+        super().__init__(pawns, turn)
+        self.move = Move(self.pawns, turn)
+        self.hit = Hit(self.pawns(), self.turn())
+
     def possible_combo(self, pawn, empty):
-        pawns_after_move = Move.move_with_hits(pawn, empty)
-        pawns_for_combo = Pawns(Board(self.length, self.width))
-        pawns_for_combo.set_actual_pawns(pawns_after_move)
+        if not self._if_can_hit(pawn, empty):
+            return False
+        pawns_after_move = self.move.move_with_hits(pawn, empty)
+        self.pawns.set_actual_pawns(pawns_after_move)
         previous_move_type = Movement.recognize_move(pawn, empty)
         new_pawn_cords = empty
-        hit_for_combo = Hit(pawns_for_combo, self.turn)
-        if new_pawn_cords in hit_for_combo.which_can_hit():
-            for empty_space in hit_for_combo.where_can_hit()[new_pawn_cords]:
+        if new_pawn_cords in self.hit.which_can_hit():
+            for empty_space in self.hit.where_can_hit()[new_pawn_cords]:
                 if Movement.recognize_move(new_pawn_cords, empty_space) != previous_move_type:
                     return True
         return False
+
+    def _if_can_hit(self, pawn, empty):
+        if pawn not in self.hit.which_can_hit():
+            return False
+        if empty not in self.hit.where_can_hit()[pawn]:
+            return False
+        return True
+
+    def make_combo(self):
+        pass
