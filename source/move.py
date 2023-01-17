55f13980ca665eb0771889cc6bd54bc76b3fdb96
dominika232323 <dominikaboguszewska62@gmail.com>
2023-01-17 20:24:31 +0100

    Added docstrings

--- source/move.py
+++ source/move.py
@@ -7,30 +7,62 @@ from source.hit import Hit
 
 
 class Move:
+    """
+    Class Move. Contains attributes:
+    :param pawns: two-dimensional list of pawns set on the board
+    :type pawns: list
+
+    :param turn: holds whose turn it is, first or second player
+    :type turn: string
+    """
     def __init__(self, pawns, turn):
+        """
+        Creates an instance of Move.
+        """
         self.hit = Hit(pawns, turn)
 
     @property
     def pawns(self):
+        """
+        :return: placement of the pawns on the board
+        """
         return self.hit.pawns()
 
     @property
     def turn(self):
+        """
+        :return: whose turn is it, first or second player
+        """
         return self.hit.turn()
 
     @property
     def pawn_to_hit(self):
+        """
+        :return: type of pawns to capture
+        """
         return self.hit.pawn_to_hit()
 
     @property
     def length(self):
+        """
+        :return: board's length
+        """
         return self.hit.length()
 
     @property
     def width(self):
+        """
+        :return: board's width
+        """
         return self.hit.width()
 
     def move_maker(self, pawn, empty, choice):
+        """
+        :param pawn: co-ordinates of a pawn that makes move
+        :param empty: co-ordinates of an empty space where the pawn moves
+        :param choice: player's choice to capture opponent's pawns by withdrawal or by approach
+        :return: two-dimensional list of pawns placement after move
+        """
         self._validate_move_maker(empty, pawn)
 
         if not self.hit.which_can_hit():
@@ -39,6 +71,14 @@ class Move:
             return self.move_with_hits(pawn, empty, choice)
 
     def _validate_move_maker(self, empty, pawn):
+        """
+        :param empty: co-ordinates of an empty space where the pawn moves
+        :param pawn: co-ordinates of a pawn that makes move
+        :raise: MoveError if the pawn cannot move
+        :raise: MoveError if the pawn cannot move to the empty space
+        :raise: MoveError if there are pawns that capture opponent's pawns but the pawn isn't one of them
+        :raise: MoveError if the pawn can capture opponent's pawns but not by moving to the empty space
+        """
         if pawn not in self.hit.which_can_move():
             raise MoveError('This pawn cannot move')
         if empty not in self.hit.where_can_move()[pawn]:
@@ -49,12 +89,23 @@ class Move:
             raise MoveError('This pawn does not have any hits here')
 
     def move_without_hits(self, pawn, empty):
+        """
+        :param pawn: co-ordinates of a pawn that makes move
+        :param empty: co-ordinates of an empty space where the pawn moves
+        :return: two-dimensional list of pawns placement after move
+        """
         pawns_after_move = self.copy_pawns()
         pawns_after_move[pawn[0]][pawn[1]] = EMPTY_COLOR
         pawns_after_move[empty[0]][empty[1]] = self.turn
         return pawns_after_move
 
     def move_with_hits(self, pawn, empty, choice):
+        """
+        :param pawn: co-ordinates of a pawn that makes move
+        :param empty: co-ordinates of an empty space where the pawn moves
+        :param choice: player's choice to capture opponent's pawns by withdrawal or by approach
+        :return: two-dimensional list of pawns placement after move
+        """
         withdrawal = self.hit.which_hits_by_withdrawal()
         approach = self.hit.which_hits_by_approach()
 
@@ -66,6 +117,12 @@ class Move:
         return self.move_with_hits_kill_pawns(pawn, empty, dead_pawns)
 
     def move_with_hits_kill_pawns(self, pawn, empty, dead_pawns):
+        """
+        :param pawn: co-ordinates of a pawn that makes move
+        :param empty: co-ordinates of an empty space where the pawn moves
+        :param dead_pawns: list of co-ordinates of opponent's pawns that are being captured
+        :return: two-dimensional list of pawns placement after move
+        """
         pawns_after_move = self.move_without_hits(pawn, empty)
 
         for dead in dead_pawns:
@@ -74,6 +131,9 @@ class Move:
         return pawns_after_move
 
     def copy_pawns(self):
+        """
+        :return: copy of two-dimensional list of pawns placement
+        """
         copied_pawns = []
 
         for row in self.pawns:
@@ -85,6 +145,11 @@ class Move:
         return copied_pawns
 
     def was_move_capturing(self, pawn, empty):
+        """
+        :param pawn: co-ordinates of a pawn that makes move
+        :param empty: co-ordinates of an empty space where the pawn moves
+        :return: True if move made by moving the pawn to the empty space was capturing
+        """
         if pawn in self.hit.which_can_hit():
             if empty in self.hit.where_can_hit()[pawn]:
                 return True
